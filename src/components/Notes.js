eaf3fe2ea04878a6b041f9ba4d67122405d39a5b
Christopher Dougherty <christophertdoc@gmail>
2020-04-30 21:33:07 -0400

    add delete function

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useQuery } from "@apollo/react-hooks"
+import { useQuery, useMutation } from "@apollo/react-hooks"
 import { gql } from "apollo-boost"
 import { withApollo } from "@apollo/react-hoc"
 
@@ -13,11 +13,31 @@ export const NOTES = gql`
   }  
 `
 
+const REMOVE_NOTE = gql`
+  mutation ($id: String) {
+    delete_notes (
+      where: {
+        id: {
+          _eq: $id
+        }
+      }
+    ) {
+      affected_rows
+    }
+  }
+`
+
 const Notes = () => {
 
   const { loading, error, data } = useQuery(NOTES)
+  const [deleteNote, { loading: deleting, error: deleteError }] = useMutation(REMOVE_NOTE)
 
-  const deleteNote = () => console.log('delete')
+  const remove = (id) => {
+    console.log('delete')
+    deleteNote({
+      variables: { id: id }
+    });
+  }
 
   if (loading) return "Loading..."
   if (error) return `Error! ${error.message}`
@@ -28,7 +48,7 @@ const Notes = () => {
         <div key={note.id}>
           <h2>{note.name}</h2>
           <p>{note.content}</p>
-          <button onClick={ () => deleteNote() }>
+          <button onClick={ () => remove(note.id) }>
             DELETE
           </button>
         </div>
