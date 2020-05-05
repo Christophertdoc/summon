6b9b5f5e72f4990e3da45ada6edcc2c4f55849bd
Christopher Dougherty <christophertdoc@gmail>
2020-05-04 21:39:19 -0400

    insert

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useQuery, useMutation } from "@apollo/react-hooks"
 import { gql } from "apollo-boost"
 import { withApollo } from "@apollo/react-hoc"
+import { Mutation } from "react-apollo"
 
 export const NOTES = gql`
   {  
@@ -12,7 +13,6 @@ export const NOTES = gql`
     }
   }  
 `
-
 const REMOVE_NOTE = gql`
   mutation ($id: String) {
     delete_notes (
@@ -27,10 +27,42 @@ const REMOVE_NOTE = gql`
   }
 `
 
+const ADD_NOTE = gql`
+  mutation AddNote($id: String!, $name: String!, $content: String!) {
+    insert_notes(objects: [{id: $id, name: $name, content: $content}]) {
+      affected_rows
+      returning {
+        id
+        name
+        content
+      }
+    }
+  }
+`
+
+// const ADD_NOTE = gql`
+//   mutation {
+//     addNote(
+//         objects: [
+//             {
+//               id: "adf",
+//               name: "ansdkfkaf",
+//               content: "ueuiruiruei"
+//             }
+//         ]
+//     ){
+//       affected_rows
+//     }
+//   }
+// `
+
 const Notes = () => {
+
+  let input
 
   const { loading, error, data } = useQuery(NOTES)
   const [deleteNote, { loading: deleting, error: deleteError }] = useMutation(REMOVE_NOTE)
+  // const [addNote, { noteData }] = useMutation(ADD_NOTE)
 
   const remove = (id) => {
     console.log('delete')
@@ -53,6 +85,42 @@ const Notes = () => {
           </button>
         </div>
       ))}
+      <Mutation mutation={ADD_NOTE}>
+
+        {/* <form
+          onSubmit={e => {
+            e.preventDefault();
+            addNote({ variables: { id: input.value, name: input.value, content: input.value } });
+            input.value = '';
+          }}
+        >
+          <input
+            ref={node => {
+              input = node;
+            }}
+          />
+          <button type="submit">Add Note</button>
+        </form> */}
+
+        {/* <button onClick={ () => {
+            addNote()  
+          }}>
+              Add note
+          </button> */}
+
+          {(addNote, {loading, data}) => {
+            return (
+              <form onSubmit={(e) => {
+                e.preventDefault()
+                console.log('submit')
+                addNote({ variables: {id: '5', name: 'hotel', content: 'Then will sun'} })
+              }}>
+                <button type="submit">submit</button>
+              </form>
+            )
+          }}
+
+      </Mutation>
     </div>
   )
 }
